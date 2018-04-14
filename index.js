848f9675f90d3d8574be529d03dfd76702ea8416
Tommy Hodgins <tomhodgins@gmail.com>
2018-04-13 20:37:16 -0400

    Refactoring to use reduce()

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 export default (selector, sibling, rule) => {
 
-  let styles = ''
-  let count = 0
+  return Array.from(document.querySelectorAll(selector))
 
-  document.querySelectorAll(selector).forEach(tag => {
+    .reduce((styles, tag, count) => {
 
-    const attr = (selector+sibling).replace(/\W/g, '')
-    const children = Array.from(tag.parentNode.querySelectorAll(sibling))
-    const index = Array.from(tag.parentNode.children).indexOf(tag)
+      const attr = (selector+sibling).replace(/\W/g, '')
+      const children = Array.from(tag.parentNode.querySelectorAll(sibling))
+      const index = Array.from(tag.parentNode.children).indexOf(tag)
 
-    children
-      .filter(child => children.indexOf(child) < index)
-      .forEach(child => {
+      children
+        .filter(child => children.indexOf(child) < index)
+        .forEach(child => {
 
-        child.setAttribute(`data-elder-${attr}`, count)
-        styles += `[data-elder-${attr}="${count}"] { ${rule} }\n`
-        count++
+          child.setAttribute(`data-elder-${attr}`, count)
+          styles += `[data-elder-${attr}="${count}"] { ${rule} }\n`
+          count++
 
-      })
+        })
 
-  })
+      return styles
 
-  return styles
+    }, '')
 
 }
